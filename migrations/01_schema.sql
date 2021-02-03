4f5916e699bb03539e1453ac1dab1011df7bc5b8
chriskang24 <christopherjkang24@gmail.com>
2021-02-03 14:04:12 -0500

    insert data into all four tables

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -36,9 +37,9 @@ CREATE TABLE reservations (
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  rating SMALLINT NOT NULL DEFAULT 0,
-  message TEXT,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
 );
